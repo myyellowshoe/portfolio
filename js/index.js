0097fe2ce4d62c64f75a24fad8c0086fbdab5782
Justin Richards <justinrichards@jmoney.attlocal.net>
2020-05-21 14:41:54 -0400

    Some small tweaks.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-
-window.addEventListener('load', function() {
-  const bodyEl = document.querySelector('body');
-  const themes = ['one', 'two', 'three', 'four', 'five'];
+window.addEventListener("load", function() {
+  const bodyEl = document.querySelector("body");
+  const themes = ["one", "two", "three", "four", "five"];
 
   setInterval(() => {
     // Filter out current theme
-    const filteredThemes = themes.filter((t) => `theme_${t}` !== bodyEl.className);
-    const selectedTheme = Math.floor(Math.random() * 6);
+    const filteredThemes = themes.filter(
+      t => `theme_${t}` !== bodyEl.className
+    );
+    const selectedTheme = Math.floor(Math.random() * filteredThemes.length);
     bodyEl.className = `theme_${filteredThemes[selectedTheme]}`;
-  }, 7000);
-})
+  }, 6000);
+});
